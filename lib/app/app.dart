fefc65277e22630fa0ee494fbf5c36e8b2ff68df
Tim <48436110+Baqiwaqi@users.noreply.github.com>
2022-02-22 11:39:27 +0100

    change app title

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -27,7 +27,7 @@ class AppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Tim Bouma's Portfolio",
       theme: ThemeData.light().copyWith(
         primaryColor: primaryColor,
         scaffoldBackgroundColor: backgroundColor,
